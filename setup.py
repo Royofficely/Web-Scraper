529c2b56f7c5c70b739612281512665440e77ffd
Roy <103624234+Royofficely@users.noreply.github.com>
2024-08-05 13:25:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
-# Reading the long description from README.md
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Reading requirements from requirements.txt
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read().splitlines()
 
@@ -17,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/roynativ/officely-web-scraper",
-    packages=["officely_web_scraper"],
+    packages=find_packages(),
     install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -35,4 +33,5 @@ setup(
             "officely-scraper=officely_web_scraper.scan:main",
         ],
     },
+    scripts=['officely-scraper'],
 )
