f692800692877bf0517ab83f9904380905d2502b
Roy <103624234+Royofficely@users.noreply.github.com>
2024-08-05 13:21:22 +0300

    Create config.py

---
+++ officely_web_scraper/config.py
@@ -0,0 +1,8 @@
+config = {
+    "domain": "https://help.officely.ai",
+    "include_keywords": None,  # Set to None to disable this filter
+    "exclude_keywords": None,  # Set to None to disable this filter
+    "max_depth": 1,  # Set to None for unlimited depth
+    "target_div": None,  # Set to None to scrape the whole page
+    "start_with": None,  # URLs must start with these substrings for exampl ["https://help.example.ai/blog"]
+}
