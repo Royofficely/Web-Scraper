23568e5716cc0495cf89ebcbfc238d630ea9d6ab
Roy Nativ <103624234+Royofficely@users.noreply.github.com>
2024-10-22 12:40:31 +0300

    Update config.py

--- officely_web_scraper/config.py
+++ officely_web_scraper/config.py
@@ -13,3 +13,32 @@ config = {
     "connections_per_host": 5,  # Maximum number of connections per host
     "delay_between_requests": 0.5,  # Delay (in seconds) between individual requests
 }
+
+
+# Option 2 - Filter by target div(example):
+
+
+#config = {
+#     "domain": "https://help.officely.ai",
+#     "include_keywords": ["officely","articles"],
+#     "exclude_keywords": None,
+#     "max_depth": 1,
+#     "target_divs": {
+#         "title": {
+#             "selector": "#main-content > section > div > div.relative.z-3.w-full.lg\:max-w-160 > div:nth-child(2) > div > div.mb-10.max-lg\:mb-6 > div > div.flex.flex-col > header",
+#             "title": "Article Title"
+#         },
+#         "description": {
+#             "selector": "#main-content > section > div > div.relative.z-3.w-full.lg\:max-w-160 > div:nth-child(2) > div > div.mb-10.max-lg\:mb-6 > div > div.flex.flex-col > div",
+#             "title": "Article Description"
+#         }
+#     },
+#     "start_with": None,
+#     "split_length": None,
+#     "excluded_protocols": ['whatsapp:', 'tel:', 'mailto:'],
+#     "max_retries": 5,
+#     "base_delay": 1,
+#     "concurrent_requests": 10,
+#     "connections_per_host": 5,
+#     "delay_between_requests": 0.5,
+# }
